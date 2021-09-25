7bfe37c4dae64eb2d10c3c1b828738de2287500f
AndriiKurapov <andreykawwee@gmail.com>
2021-09-25 12:04:48 +0300

    Update CalculatorServlet.java

--- src/main/java/CalculatorServlet.java
+++ src/main/java/CalculatorServlet.java
@@ -1,8 +1,5 @@
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.*;
+import javax.servlet.http.*;
 import java.io.IOException;
 
 /**
